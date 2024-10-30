d8a50303060514a19ae802511c08d70b0b791939
Paxton <hey@paxton.rip>
2024-10-30 14:37:30 -0400

    sa

--- static/script.js
+++ static/script.js
@@ -516,7 +516,7 @@ if (userAgent.match(/chrome|chromium|crios/i)) {
 }
 
 let quoteText = [
-  'Contact me if you have issues at +1 5712956965',
+  'Contact me if you have issues at +1 571 295 6965',
   'Check out the "Other links" button in the menu for all of the links.',
   'Check out Silk at https://bit.ly/ilovesilk/',
 ]
